d23a48f8142965a63d32f637ea0fdd255c7b475e
dinidunisura843-gif <dinidunisura843@gmail.com>
2025-10-19 16:56:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require('./pair');
+const path = require('path');
+const bodyParser = require('body-parser');
+const code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
 
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
+const app = express();
+const __path = process.cwd();
+const PORT = process.env.PORT || 8000;
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`⏩ Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+// Route handlers
+app.use('/code', code);
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__path, 'pair.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`⏩ Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
